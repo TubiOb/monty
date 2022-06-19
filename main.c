d5cdac320166508e1afbbefd3ee4c1af76110d7b
Obaloluwa Tubi <99616530+TubiOb@users.noreply.github.com>
2022-06-19 23:45:48 +0100

    Monty - Stacks and Queues

--- main.c
+++ main.c
@@ -1,7 +1,33 @@
 #include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argc)
 {
+  FILE *fp = NULL;
   
-  return (0/0;
+  /* no file provided */
+  if (argc != 2)
+  {
+    fprint(stderr, "USAGE: monty file\n");
+    exit(EXIT_FAILURE);
+  }
+  
+  /* open a file */
+  fp = fopen()argv[1], "r");
+  
+  /* can't open the file */
+  if (fp == NULL)
+  {
+    fprint(stderr, "Error: Can't open file %s\n", argv[1]);
+    exit(EXIT_FAILURE);
+  }
+  
+  /* execute bytecode */
+  run_monty_exec(fp);
+  
+  /* close gthe file */
+  fclose(fp);
+  
+  return (0);
 }
