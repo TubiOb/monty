f6d91836d263f8c6a00a9914793ce8a8a10c285a
Obaloluwa Tubi <99616530+TubiOb@users.noreply.github.com>
2022-06-20 01:31:12 +0100

    Monty - Stacks and Queues

--- run_monty_exec.c
+++ run_monty_exec.c
@@ -9,16 +9,36 @@ void run_monty_exec(FILE *fp)
   size_t line_size;
   ssize_t len;
   char *op_code = NULL, *op_code_arg = NULL;
+  size_t line_number = 0;
+  int_op_code_arg;
+  stack_t *stack = NULL;
   
   /* read a line */
   while ((len = getline(&line, &line_size, fp)) != -1)
   {
     line[len - 1] = '\0';
+    line_number += 1;
     
     op_code = strtok(line, " ");
     op_code_arg = strtok(NULL, " ");
     
     /* find the opcode handler */
-    
+    if (strcmp(op_code, "push") == 0)
+    {
+      /* cheeck if arg is an int */
+      if (!isnumeric(op_code_arg))
+      {
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+      }
+      
+      int_op_code_arg = atoi(op_code_arg);
+      push(&stack, int_op_code_arg);
+    }
+    else
+    {
+      /* get the op_code handler */
+      /* execute the op_code hanlder */
+    }
   }
 }
