a40d2d7733890660c388255931c9c431cd88c03a
Obaloluwa Tubi <99616530+TubiOb@users.noreply.github.com>
2022-06-20 04:27:19 +0100

    Monty - Stacks and Queues

--- select_op_code_handler.c
+++ select_op_code_handler.c
@@ -2,5 +2,14 @@
 
 void (*select_op_code_handler(char *op_code))(stack_t **, unsigned int)
 {
+    if (strcmp(op_code, "pall") == 0)
+        return (op_pall);
+               
+    if (strcmp(op_code, "pint") == 0)
+        return (op_pint);
+                    
+    if (strcmp(op_code, "pop") == 0)
+        return (op_pop);
   
+    return (NULL);
 }
