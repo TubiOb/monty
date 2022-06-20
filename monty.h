b7607456f3fc288ebda34632b6e06ffe68aea02d
Obaloluwa Tubi <99616530+TubiOb@users.noreply.github.com>
2022-06-20 01:38:27 +0100

    Monty - Stacks and Queues

--- monty.h
+++ monty.h
@@ -37,6 +37,8 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-
+/* op_code handlers */
+void op_pall(stack_t **stack, unsigned int line_number);
+void op_pint(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
