6a8a05fa031d68d66de8275a34e098fed0076e11
Obaloluwa Tubi <99616530+TubiOb@users.noreply.github.com>
2022-06-20 03:04:44 +0100

    Monty - Stacks and Queues

---
+++ op_push.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void op_push(stack_t **stack, unsigned int line_number, char *arg)
+{
+  /* check if arg is an int */
+  if (!is_numeric(arg))
+  {
+    fprintf(stderr, "L%d: usage: push integer\n", line_number);
+    exit(EXIT_FAILURE);
+  }
+  
+  push(&stack, atoi(arg));
+}
