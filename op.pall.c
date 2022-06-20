8f1ed62eacf3b58ea22d855f8b178a80ccf0535e
Obaloluwa Tubi <99616530+TubiOb@users.noreply.github.com>
2022-06-20 01:58:50 +0100

    Monty - Stacks and Queues

---
+++ op.pall.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+#include <stdio.h>
+
+void op_pall(stack_t **stack, unsigned int line_number)
+{
+  stack_t *cursor = *stack;
+  
+  while(*cursor)
+  {
+    printf("%d\n", cursor->n);
+    cursor = cursor->next;
+  }
+}
