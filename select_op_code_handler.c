ab8f0bd571491914a8450ec4e1e70347ce079e56
Obaloluwa Tubi <99616530+TubiOb@users.noreply.github.com>
2022-06-20 04:48:46 +0100

    Monty - Stacks and Queues

--- select_op_code_handler.c
+++ select_op_code_handler.c
@@ -3,14 +3,19 @@
 
 void (*select_op_code_handler(char *op_code))(stack_t **, unsigned int)
 {
-    if (strcmp(op_code, "pall") == 0)
-        return (op_pall);
-               
-    if (strcmp(op_code, "pint") == 0)
-        return (op_pint);
-                    
-    if (strcmp(op_code, "pop") == 0)
-        return (op_pop);
-  
-    return (NULL);
+    instruction_t ops[] = {
+        {"pall", op_pall},
+        {"pint", op_pint},
+        {"pop", op_pop}
+    };
+    size_t len = sizeof(ops) / sizeof(instruction_t);
+    size_t i = 0;
+    
+    while (i < len && strcmp(ops[i].opcode, op_code != 0)
+           i++;
+           
+    if (i == len)
+           return (NULL);
+           
+    return (ops[i].f);
 }
